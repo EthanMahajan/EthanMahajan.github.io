963add45297d3b22022fe099213b2bc1fcdc6a1a
EthanMahajan <62560232+EthanMahajan@users.noreply.github.com>
2026-03-20 20:14:34 +0000

    Deploying to gh-pages from @ EthanMahajan/EthanMahajan.github.io@89b26df6e1c2161f8c8ccc3ae56eae3ace417ff7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,11 +408,6 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-artorius",
           title: 'Artorius',
           description: "High-Speed, Reliable coreXY 3D Printer",
