ba94ed35170bcce8f5376f199740df29406ae0e1
EthanMahajan <62560232+EthanMahajan@users.noreply.github.com>
2026-03-21 03:15:23 +0000

    Deploying to gh-pages from @ EthanMahajan/EthanMahajan.github.io@8a7f7ab21750c3284ecca6eda8e068f933e4e7ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "list of projects I have worked on",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
